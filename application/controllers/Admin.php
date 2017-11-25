b7b75cab9afa432c83482bec25e0d4a4cbb551ac
krimstix <kedren.villena@gmail.com>
2017-11-26 00:18:18 +0800

    Implemented navbar links

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -88,7 +88,8 @@ class Admin extends MY_Controller
                 //$this->admin_data = $this->set_custom_session->admin_session();
                 
                 //$this->load->view('admin/dashboard');
-                redirect('/Form');
+                $this->render($this->set_views->admin_home());
+                //redirect('/Form');
             }// user true
             else
             {
