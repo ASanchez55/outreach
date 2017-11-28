ef0165daa3aabf3bafbc1453db42c1669daf96b4
keeed <kedren.villena@gmail.com>
2017-11-28 13:23:40 +0800

    code segregation for create family

---
+++ application/libraries/viewbag.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ViewBag 
+{
+    public function header()
+    {
+        return 'layout/header';
+    }
+    
+    public function footer()
+    {
+        return 'layout/footer';
+    }
+
+    public function navbar()
+    {
+        return 'layout/navbar';
+    }
+
+    public function family_create()
+    {
+        return 'family/create';
+    }
+
+
+	public function admin_header()
+	{
+		return 'layout/header';
+	}
+
+	public function admin_footer()
+	{
+		return 'layout/footer';
+	}
+
+	public function admin_home()
+	{
+		return 'admin/home';
+	}
+
+	public function form_family()
+	{
+		return 'create_family';
+	}
+
+	public function form_participant()
+	{
+		return 'add_family_member';
+	}
+
+	public function form_success()
+	{
+		return 'form_success';
+	}
+
+	public function form_event()
+	{
+		return 'create_event';
+	}
+
+	public function ajax()
+	{
+		return 'js/ajax';
+	}
+
+	public function login()
+	{
+		return 'user_login';
+	}
+
+	public function search_family()
+	{
+		return 'family_search';
+	}
+
+	public function form_register_family_event()
+	{
+		return 'register_family_event';
+	}
+
+
+}
