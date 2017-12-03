d9b38815ede827f2f0b75036ec4fbba556072e53
krimstix <kedren.villena@gmail.com>
2017-12-04 00:13:37 +0800

    #9 Added proposed SQL Scripts. Used a more conventional table naming style.

---
+++ sql/outreach.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 03, 2017 at 05:02 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `outreach`
+--
+CREATE DATABASE IF NOT EXISTS `outreach` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `outreach`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `valid` tinyint(1) NOT NULL DEFAULT '1',
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+`id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `date` date NOT NULL,
+  `max_participants` int(11) NOT NULL,
+  `event_date` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events_families`
+--
+
+DROP TABLE IF EXISTS `events_families`;
+CREATE TABLE IF NOT EXISTS `events_families` (
+`id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `family_id` int(11) NOT NULL,
+  `date_registered` date DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events_family_members`
+--
+
+DROP TABLE IF EXISTS `events_family_members`;
+CREATE TABLE IF NOT EXISTS `events_family_members` (
+`id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `family_id` int(11) NOT NULL,
+  `family_member_id` int(11) NOT NULL,
+  `date_attended` date DEFAULT NULL,
+  `attend` tinyint(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `families`
+--
+
+DROP TABLE IF EXISTS `families`;
+CREATE TABLE IF NOT EXISTS `families` (
+`id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` text NOT NULL,
+  `registration_number` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `family_members`
+--
+
+DROP TABLE IF EXISTS `family_members`;
+CREATE TABLE IF NOT EXISTS `family_members` (
+`id` int(11) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `family_id` int(11) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `birth_date` date NOT NULL,
+  `head_family` tinyint(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events_families`
+--
+ALTER TABLE `events_families`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `events_family_members`
+--
+ALTER TABLE `events_family_members`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `families`
+--
+ALTER TABLE `families`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `family_members`
+--
+ALTER TABLE `family_members`
+ ADD PRIMARY KEY (`id`);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
